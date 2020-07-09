562a82ae1632c528879f2064150df0b10399954b
Omar Mansour <omarsayedmohamedmansour@gmail.com>
2020-07-10 00:46:01 +0200

    finished home-page working on menu

---
+++ src/pages/header.js
@@ -0,0 +1,14 @@
+<header class="header">
+<div class="left">
+    <a href="#" class="nav-item">Home</a>
+    <a href="#" class="nav-item">Menu</a>
+    <a href="#" class="nav-item">Chef</a>
+</div>
+<div class="middle">
+    <img src="../src/assets/logo.png" alt="Chotto Motto Logo" id="logo">
+</div>
+<div class="right">
+    <a href="#" class="nav-item">Reservations</a>
+    <a href="#" class="nav-item">Location</a>
+</div>
+</header>
---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+<div class="home">
+            <div class="intro">
+                <h1 class="title">
+                    555 Wellington St. <br> Hong Kong
+                </h1>
+                <div class="welcome-image">
+                    <img src="../src/assets/image-asset.jpeg" alt="Plate of Noodles" id="home-image"/>
+                </div>
+                <h1 class="hours">
+                    12:00 to 0:00
+                    <br>Every day
+                </h1>
+            </div>
+            <div class="links-contact">
+                <div class="links">
+                    <a href="#" class="link-item">Menu</a> •
+                    <a href="#" class="link-item">Chef</a>  •
+                    <a href="#" class="link-item">Reservations</a>  •
+                    <a href="#" class="link-item">Location</a>
+                </div>
+                <div class="number">
+                    <number>+ 852 1234 5678</number>
+                </div>
+            </div>
+        </div>
